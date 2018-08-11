a62d1b34c55e519aaaf916c3f09ff5790512dd00
gcofficial <gurievcreative@gmail.com>
2018-08-11 12:02:06 +0300

    init

---
+++ src/vue-tree.vue
@@ -0,0 +1,56 @@
+<script>
+export default {
+  name: 'VueTree',
+  props: {
+    model: Object,
+    prepareFolderNameFunc: {
+      type: Function,
+      default: function(name, open) {
+        return open ? `[-] ${ name }` : `[+] ${ name }`;
+      },
+    },
+  },
+  data() {
+    return {
+      open: false,
+    };
+  },
+  computed: {
+    isFolder() {
+      return this.model.children && this.model.children.length;
+    },
+  },
+  methods: {
+    toggle() {
+      if (this.isFolder) {
+        this.open = !this.open;
+      }
+    },
+  },
+};
+</script>
+
+<template>
+  <li>
+    <div v-if="!isFolder">
+      {{ model.name }}
+    </div>
+    <div
+      v-if="isFolder"
+      @click="toggle"
+      class="bold">
+      {{ prepareFolderNameFunc(model.name, open) }}
+    </div>
+
+    <ul v-show="open" v-if="isFolder">
+      <VueTree
+        class="item"
+        v-for="(model, index) in model.children"
+        :key="index"
+        :model="model"
+        :prepareFolderNameFunc="prepareFolderNameFunc"></VueTree>
+    </ul>
+  </li>
+</template>
+
+<style scoped></style>
